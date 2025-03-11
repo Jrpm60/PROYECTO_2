1f60c9fc26127b5c1fd525a8c3310a20e13c1826
Jrpm60 <quantumlog.66@gmail.com>
2025-03-11 13:06:58 +0100

    cas_tue

--- indexEJS.js
+++ indexEJS.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import path from 'path';
-import { fileURLToPath } from 'url';  // __dirname no esta disponible con ES Modules
+import { fileURLToPath } from 'url';
 import fs from 'fs'
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,117 +8,137 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Incluir el middleware static
-// Recordar que los htmls no estan dentro de /public/css y /public/images y /public/js
-// Colgar los html en una carpeta views
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-// middleware _____________________________
 function logger(req, res, next) {
     console.log(`LOGGER: ${req.url}`);
     next();
 }
-app.use(logger) 
+app.use(logger)
 
-// Middleware ______________________________
+/* INICIO BLOQUE DE NAVEGACION */
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'inicio.ejs')); 
+/* app.get('/2_0_inicio', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', '2_0_inicio.ejs'));
+}); */
+
+app.get('/blanco', (req, res) => {
+    res.render("0_0_blanco");
 });
 
 app.get('/inicio', (req, res) => {
-    res.render("inicio"); 
+    res.render("2_0_inicio");
 });
 
+app.get('/login', (req, res) => {
+    res.render("2_1_login");
+});
 
-app.get('/parcial', (req, res) => {
-    res.render("parcial"); 
+app.get('/form', (req, res) => {
+    res.render("2_2_form");
+});
+
+app.get('/contact', (req, res) => {
+    res.render("2_3_contact");
 });
 
 app.get('/cliHist', (req, res) => {
-    res.render("cliHist"); 
+    res.render("2_4_cliHist");
 });
 
-
-
-
-
-
-
-app.get('/pedido1', (req, res) => {
-
-    const data = {
-        header: 'Header',
-        titulo : "Mi primer Titulo"
-     };
-
-    const isLoggedIn = false;
-    res.render("pedido1", {data, isLoggedIn});
+app.get('/regInicio', (req, res) => {
+    res.render("2_10_regInicio");
 });
 
-app.post('/pedido', (req, res) => {
-    console.log('POST');
+app.get('/gracias', (req, res) => {
+    res.render("2_9_thanks");
+});
 
-    const {articulo, cantidad, presupuesto, email } = req.body;
+/* FIN BLOQUE NAVEGACION */
 
-    const resp = {
-        mensaje1 :"",
-        mensaje2 :"",
-        mensaje3 :"",
+app.post('/contact', (req, res) => {
+    const filePath = 'data_contact_form.txt';
+    const redirectUrl = '/gracias';
+    const now = new Date();
+    const dateTimeString = now.toLocaleString();
+    const dataString = `${dateTimeString}, ${Object.values(req.body).join(', ')}\n`;
+
+    fs.appendFile(filePath, dataString, err => {
+        if (err) {
+            res.status(500).send('Error al guardar los datos');
+        } else {
+            res.redirect(redirectUrl);
+        }
+    });
+});
+
+app.post('/procesar', (req, res) => {
+    const filePath = 'data_job_application.txt';
+    const redirectUrl = '/gracias';
+    const now = new Date();
+    const dateTimeString = now.toLocaleString();
+    const dataString = `${dateTimeString}, ${Object.values(req.body).join(', ')}\n`;
+
+    fs.appendFile(filePath, dataString, err => {
+        if (err) {
+            res.status(500).send('Error al guardar los datos');
+        } else {
+            res.redirect(redirectUrl);
+        }
+    });
+});
+
+app.post('/procesLogin', (req, res) => {
+    const filePath = 'data_login.txt';
+    const { usuario, password } = req.body;
+    let redirectUrl;
+
+    if (usuario === 'pp' && password === '11') {
+        var isLoggedIn = true;
+        redirectUrl = "/regInicio";
+    } else {
+        var isLoggedIn = false;
+        redirectUrl = '/gracias';
+    
     }
 
-    console.log(`articulo ${articulo}`)
-    console.log(`cantidad ${cantidad}`)
-    console.log(`presupuesto ${presupuesto}`)
-    console.log(`email ${email}`)
+    const now = new Date();
+    const dateTimeString = now.toLocaleString();
+    const dataString = `${dateTimeString}, ${usuario}, ${password}, ${isLoggedIn}\n`;
 
-  /*const now = new Date();
-    const dateTimeString =now.toLocaleString();
-    const dataString =`${dateTimeString}, ${validacion}, ${Object.values(formData).join(', ')}\n`;
-    console.log(`dataString ${dataString}`)
- */
-
-    if (presupuesto  > 100 && presupuesto < 501) {
-            var validacion="ok";
-            resp.mensaje1 = "Datos Recibidos" 
-            resp.mensaje2 = "Pedido registrado correctamente" 
-            resp.mensaje3 = "Gracias, estamos a tu disposicion para nuevos pedidos"      
-            //res.json({Resultado: 'Pedido registrado Correctamente'});
-            //res.sendFile(path.join(__dirname, 'views', 'registro_usu_priv.html'));
-            res.render("registro_usu_estado", {resp} )
+    fs.appendFile(filePath, dataString, err => {
+        if (err) {
+            res.status(500).send('Error al guardar los datos');
+        } else {
+            res.redirect(redirectUrl);
         }
-        else {
-            var validacion="nok";
-            resp.mensaje1 = "Datos Recibidos" 
-            resp.mensaje2 = "Pedido NO Registrado"
-            resp.mensaje3 = "Corrige tus datos"
-
-            //res.json({Resultado: 'El presupuesto debe ser mayor de 100 y menor de 500'});
-            //res.sendFile(path.join(__dirname, 'views', 'registro_usu_rech.html'));
-            res.render("registro_usu_estado", {resp} )
-        }
-         
     });
+});
 
-    app.get('/welcome',(req,res) => {
-        
-        const user = {
-            isAdmin: true
+app.post('/cliHist', (req, res) => {
+    const filePath = 'data_cliHist.txt';
+    const redirectUrl = '/gracias';
+    const now = new Date();
+    const dateTimeString = now.toLocaleString();
+    const dataString = `${dateTimeString}, ${Object.values(req.body).join(', ')}\n`;
+
+    fs.appendFile(filePath, dataString, err => {
+        if (err) {
+            res.status(500).send('Error al guardar los datos');
+        } else {
+            res.redirect(redirectUrl);
         }
-        res.render('welcome', {user});
+    });
+});
 
 
-    })
 
-
-    
 app.use((req, res) => {
-    res.status(404).send('Error pagina no existe')
+    res.status(404).send('Error pagina no existe');
 });
 
 app.listen(3000, () => {
